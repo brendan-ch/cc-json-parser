1a9b9332c73d8db4e918a11bcbdc5014b9818580
Brendan Chen <me@bchen.dev>
2026-04-17 10:32:11 -0700

    Add object detection

--- src/parse.cpp
+++ src/parse.cpp
@@ -1,14 +1,19 @@
 #include "parse.hpp"
 #include <iostream>
 
-int parse(const string &json)
+int parse_object(const string &json)
+{
+    return 0;
+}
+
+string trim(const string &json)
 {
     int start = 0;
     int end = json.length();
     cout << start << " " << end << endl;
 
     if (start >= end)
-        return 1;
+        return "";
 
     char start_char = json.at(start);
     while (start < end - 1 && (start_char == ' ' || start_char == '\n'))
@@ -24,17 +29,30 @@ int parse(const string &json)
         end_char = json.at(end - 1);
     }
 
-    if (start_char == '{')
+    auto substr = json.substr(start, end - start);
+    return substr;
+}
+
+int parse(const string &json)
+{
+    auto substr = trim(json);
+    if (substr.length() == 0) return 1;
+
+    char start_char = substr.at(0);
+    char end_char = substr.at(substr.length() - 1);
+
+    if (start_char == '{' && end_char == '}')
     {
-        // parse object
+        return parse_object(substr);
     }
-    else if (start_char == '"')
+    else if (start_char == '"' && end_char == '}')
     {
-        // save as string literal
+        return 0;
     }
+
     // also include cases for keyword literals (true/false/null) and numbers (ints + floats)
 
     // cout << start << " " << end << endl;
 
-    return 0;
+    return 1;
 }
